287dbb77ecb4db6e14a466ff440486cd380528f6
Artem Lira <artemlira84@gmail.com>
2024-01-03 19:45:10 +0200

    change limit '5mb'

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ import { handleValidationErrors, checkAuth } from './utils/index.js';
 const app = express();
 dotenv.config();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb' }));
 app.use('/uploads', express.static('uploads'));
 app.set('view engine', 'ejs');
 
